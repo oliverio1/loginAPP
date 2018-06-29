7d5bd5176ef464fdf8f90fcdc1bce59e5e753ebc
Oliver Martinez <oliverio.oo@gmail.com>
2018-06-29 05:29:39 -0500

    Agregando Firebase y Angularfire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyDbImSZsUnqBeuyGXOTXZLfdqCy8lHpe0w",
+        authDomain: "contacto-d367b.firebaseapp.com",
+        databaseURL: "https://contacto-d367b.firebaseio.com",
+        projectId: "contacto-d367b",
+        storageBucket: "contacto-d367b.appspot.com",
+        messagingSenderId: "924152378344"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+    firebaseConfig: {
+        apiKey: "AIzaSyDbImSZsUnqBeuyGXOTXZLfdqCy8lHpe0w",
+        authDomain: "contacto-d367b.firebaseapp.com",
+        databaseURL: "https://contacto-d367b.firebaseio.com",
+        projectId: "contacto-d367b",
+        storageBucket: "contacto-d367b.appspot.com",
+        messagingSenderId: "924152378344"
+    }
 };
 
 /*
